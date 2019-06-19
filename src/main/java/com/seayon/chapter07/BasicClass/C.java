ea87df85af01e1d3f5a6dae30a529c9beab4f578
153660639@qq.com <153660639@qq.com>
2019-06-20 00:26:55 +0800

    chapter07 practice 02

--- src/main/java/com/seayon/chapter07/BasicClass/C.java
+++ src/main/java/com/seayon/chapter07/BasicClass/C.java
@@ -4,6 +4,9 @@ import static net.mindview.util.Print.print;
 
 //{CompileTimeError}(Won't compile)
 public class C extends A {
+//	public static final String B = A+"2";
+//	public static final String A = "2"; //必须有先后顺序
+
 	public C(String name) {
 		super(name);
 		print("C constructor");
