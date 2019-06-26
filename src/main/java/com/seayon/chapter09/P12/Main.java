ac02620a06a6ab60d6d010fd31783549bcfeefab
153660639@qq.com <153660639@qq.com>
2019-06-27 02:40:56 +0800

    chapter09 practice 04

---
+++ src/main/java/com/seayon/chapter09/P12/Main.java
@@ -0,0 +1,32 @@
+package com.seayon.chapter09.P12;
+
+public class Main {
+    public static void t(Base.CanFight x) {
+        x.fight();
+    }
+
+    public static void u(Base.CanSwim x) {
+        x.swim();
+    }
+
+    public static void v(Base.CanFly x) {
+        x.fly();
+    }
+
+    public static void c(Base.CanClimb x) {
+        x.climb();
+    }
+
+    public static void w(Base.ActionCharacter x) {
+        x.fight();
+    }
+
+    public static void main(String[] args) {
+/*        Base.Hero h = new Base.Hero();
+        t(h); // Treat it as a CanFight
+        u(h); // Treat it as a CanSwim
+        v(h); // Treat it as a CanFly
+        c(h); // Treat it as a CanClimb
+        w(h); // Treat it as an ActionCharacter*/
+    }
+}
