dc47cea370f7e3dac30d0fea14f4b6c17f6faecc
153660639@qq.com <153660639@qq.com>
2019-07-10 00:20:19 +0800

    chapter12 practice

---
+++ src/main/java/com/seayon/chapter12/Ex8.java
@@ -0,0 +1,18 @@
+package com.seayon.chapter12;
+import com.seayon.chapter05.Apple;
+
+/**
+ * @Version 1.0
+ * @author: 赵旭阳
+ * @Date: 2019/7/8 14:09
+ * @Description
+ */
+public class Ex8 {
+	public static void main(String[] args)throws Exception{
+
+			Apple apple=null;
+			apple.testt();
+
+
+	}
+}
---
+++ src/main/java/com/seayon/chapter12/Ex9.java
@@ -0,0 +1,54 @@
+package com.seayon.chapter12;
+
+import com.seayon.chapter05.Apple;
+
+/**
+ * @Version 1.0
+ * @author: 赵旭阳
+ * @Date: 2019/7/8 14:09
+ * @Description
+ */
+class MyEx1 extends Exception {
+    private String msg;
+
+    public MyEx1(String msg) {
+        this.msg = msg;
+    }
+}
+
+class MyEx2 extends Exception {
+    private String msg;
+
+    public MyEx2(String msg) {
+        this.msg = msg;
+    }
+}
+
+class MyEx3 extends Exception {
+    private String msg;
+
+    public MyEx3(String msg) {
+        this.msg = msg;
+    }
+}
+
+public class Ex9 {
+    public static void main(String[] args) {
+        try {
+            if (true) {
+                throw new MyEx1("this is MyEx1");
+            }
+            if (true) {
+                throw new MyEx2("this is MyEx2");
+            }
+            if (true) {
+                throw new MyEx3("this is MyEx3");
+            }
+        } catch (Exception e) {
+            System.out.println("Cause exception");
+            e.printStackTrace(System.out);
+        }
+
+
+    }
+}
